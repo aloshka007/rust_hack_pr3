4808055cf346a6b3d44d4f59c91c653b38f47e1f
aloshka007 <o.grechan@e-u.edu.ua>
2024-11-18 20:35:59 +0300

    5) staircase.rs

---
+++ 5) staircase.rs
@@ -0,0 +1,21 @@
+use std::io::{self, BufRead};
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+fn staircase(n: i32) {
+    for i in 1..=n {
+        println!("{:>width$}", "#".repeat(i as usize), width = n as usize);
+    }
+}
+#[test]
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+    staircase(n);
+}
