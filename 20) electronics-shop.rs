bd4b3a6faf39ddb4133364d2656bd5dbfb2e0a09
aloshka007 <o.grechan@e-u.edu.ua>
2024-11-18 21:28:35 +0300

    creat

--- 20) electronics-shop.rs
+++ 20) electronics-shop.rs
@@ -1,98 +1,52 @@
-#include <bits/stdc++.h>
+use std::io;
 
-using namespace std;
+fn get_money_spent(keyboards: &[i32], drives: &[i32], b: i32) -> i32 {
+    let mut max_spent = -1;
 
-vector<string> split_string(string);
-
-/*
- * Complete the getMoneySpent function below.
- */
-int getMoneySpent(vector<int> keyboards, vector<int> drives, int b) {
-    int maxSpent = -1; 
-    
-    for (int k : keyboards) {
-        for (int d : drives) {
-            int total = k + d;
-            if (total <= b) {
-                maxSpent = max(maxSpent, total);
+    for &k in keyboards {
+        for &d in drives {
+            let total = k + d;
+            if total <= b {
+                max_spent = max_spent.max(total);
             }
         }
     }
+
+    max_spent
+}
+
+fn main() {
     
-    return maxSpent;
-}
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let bnm: Vec<i32> = input
+        .trim()
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
 
-int main() {
-    ofstream fout(getenv("OUTPUT_PATH"));
+    let b = bnm[0];
+    let n = bnm[1];
+    let m = bnm[2];
 
-    string bnm_temp;
-    getline(cin, bnm_temp);
+    
+    input.clear();
+    io::stdin().read_line(&mut input).unwrap();
+    let keyboards: Vec<i32> = input
+        .trim()
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
 
-    vector<string> bnm = split_string(bnm_temp);
+   
+    input.clear();
+    io::stdin().read_line(&mut input).unwrap();
+    let drives: Vec<i32> = input
+        .trim()
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
 
-    int b = stoi(bnm[0]);
-    int n = stoi(bnm[1]);
-    int m = stoi(bnm[2]);
-
-    string keyboards_temp_temp;
-    getline(cin, keyboards_temp_temp);
-
-    vector<string> keyboards_temp = split_string(keyboards_temp_temp);
-    vector<int> keyboards(n);
-
-    for (int keyboards_itr = 0; keyboards_itr < n; keyboards_itr++) {
-        int keyboards_item = stoi(keyboards_temp[keyboards_itr]);
-        keyboards[keyboards_itr] = keyboards_item;
-    }
-
-    string drives_temp_temp;
-    getline(cin, drives_temp_temp);
-
-    vector<string> drives_temp = split_string(drives_temp_temp);
-    vector<int> drives(m);
-
-    for (int drives_itr = 0; drives_itr < m; drives_itr++) {
-        int drives_item = stoi(drives_temp[drives_itr]);
-        drives[drives_itr] = drives_item;
-    }
-
-    /*
-     * The maximum amount of money she can spend on a keyboard and USB drive, 
-     * or -1 if she can't purchase both items
-     */
-    int moneySpent = getMoneySpent(keyboards, drives, b);
-
-    fout << moneySpent << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-vector<string> split_string(string input_string) {
-    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-        return x == y and x == ' ';
-    });
-
-    input_string.erase(new_end, input_string.end());
-
-    while (input_string[input_string.length() - 1] == ' ') {
-        input_string.pop_back();
-    }
-
-    vector<string> splits;
-    char delimiter = ' ';
-
-    size_t i = 0;
-    size_t pos = input_string.find(delimiter);
-
-    while (pos != string::npos) {
-        splits.push_back(input_string.substr(i, pos - i));
-        i = pos + 1;
-        pos = input_string.find(delimiter, i);
-    }
-
-    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-    return splits;
+    let result = get_money_spent(&keyboards, &drives, b);
+    println!("{}", result);
 }
